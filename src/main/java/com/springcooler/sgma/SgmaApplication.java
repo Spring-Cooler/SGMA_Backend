e83e29dc5f24bd53e89e7b4f259067bf66fb5de5
minseokKim6823 <alstjr6823@naver.com>
2024-08-27 11:31:18 +0900

    spring boot settings

---
+++ src/main/java/com/springcooler/sgma/SgmaApplication.java
@@ -0,0 +1,13 @@
+package com.springcooler.sgma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgmaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springcooler/sgma/SgmaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcooler.sgma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgmaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
