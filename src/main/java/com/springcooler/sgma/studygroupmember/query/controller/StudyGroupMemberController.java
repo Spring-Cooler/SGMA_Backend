b189c678e783af87c31463c587a87964ea898c3b
Kibeom Jeon <kibeom1145@gmail.com>
2024-08-28 23:31:54 +0900

    Feat: 스터디 그룹원 단건 조회(그룹원 아이디), 스터디 그릅원 그룹별 조회 API 추가

--- src/main/java/com/springcooler/sgma/studygroupmember/query/controller/StudyGroupMemberController.java
+++ src/main/java/com/springcooler/sgma/studygroupmember/query/controller/StudyGroupMemberController.java
@@ -1,10 +1,65 @@
 package com.springcooler.sgma.studygroupmember.query.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.springcooler.sgma.studygroupmember.query.common.ResponseMessage;
+import com.springcooler.sgma.studygroupmember.query.service.StudyGroupMemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @RestController("queryStudyGroupMemberController")
 @RequestMapping("/api/study-group/members")
 public class StudyGroupMemberController {
 
+    StudyGroupMemberService studyGroupMemberService;
+
+    @Autowired
+    public StudyGroupMemberController(StudyGroupMemberService studyGroupMemberService) {
+        this.studyGroupMemberService = studyGroupMemberService;
+    }
+
+    @GetMapping("/{memberId}")
+    public ResponseEntity<ResponseMessage> findStudyGroupMemberByMemberId(@PathVariable long memberId) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(new MediaType("application", "json"
+                , StandardCharsets.UTF_8));
+
+        Map<String, Object> responseMap = studyGroupMemberService.findStudyGroupMemberByMemberId(memberId)
+                .stream()
+                .collect(
+                        Collectors.toMap(
+                                studyGroupMember -> String.valueOf(studyGroupMember.getMemberId()),
+                                Function.identity()
+                        )
+                );
+
+        ResponseMessage responseMessage = new ResponseMessage(200, "조회 성공!", responseMap);
+        return new ResponseEntity<>(responseMessage, headers, HttpStatus.OK);
+    }
+
+    @GetMapping("/group-id/{groupId}")
+    public ResponseEntity<ResponseMessage> findStudyGroupMembersByGroupId(@PathVariable long groupId) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(new MediaType("application", "json"
+                , StandardCharsets.UTF_8));
+
+        Map<String, Object> responseMap = studyGroupMemberService.findStudyGroupMembersByGroupId(groupId)
+                .stream()
+                .collect(
+                        Collectors.toMap(
+                                studyGroupMember -> String.valueOf(studyGroupMember.getMemberId()),
+                                Function.identity()
+                        )
+                );
+
+        ResponseMessage responseMessage = new ResponseMessage(200, "조회 성공!", responseMap);
+        return new ResponseEntity<>(responseMessage, headers, HttpStatus.OK);
+    }
 }
