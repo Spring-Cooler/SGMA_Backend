dc6c0d93efb302d4410dfc15ae83e2edd7e5761f
woodart8 <kibeom1145@gmail.com>
2024-08-28 18:03:50 +0900

    Fix: Unused Import 삭제

--- src/main/java/com/springcooler/sgma/studygroup/command/domain/repository/StudyGroupRepository.java
+++ src/main/java/com/springcooler/sgma/studygroup/command/domain/repository/StudyGroupRepository.java
@@ -3,8 +3,5 @@ package com.springcooler.sgma.studygroup.command.domain.repository;
 import com.springcooler.sgma.studygroup.command.domain.aggregate.StudyGroup;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface StudyGroupRepository extends JpaRepository<StudyGroup, Long> {
-    StudyGroup findByGroupName(String groupName);
 }
