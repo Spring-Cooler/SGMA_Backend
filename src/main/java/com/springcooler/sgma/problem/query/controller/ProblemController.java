c6c355f9cb8e68d7ed3db549c036b1f4f4c3e12b
Seohyun Kim <153896425+1etterh@users.noreply.github.com>
2024-08-29 19:36:49 +0900

    Feat: 문제 조회 API 작성 완료

--- src/main/java/com/springcooler/sgma/problem/query/controller/ProblemController.java
+++ src/main/java/com/springcooler/sgma/problem/query/controller/ProblemController.java
@@ -15,6 +15,7 @@ import java.nio.charset.Charset;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController("queryProblemController")
 @RequestMapping("/api/problems")
@@ -29,16 +30,40 @@ public class ProblemController {
         this.problems = problemService.findAllProblems();
     }
     @GetMapping("/")
-    public ResponseEntity<ResponseMessage> findAllProblems() {
+    public ResponseEntity<ResponseMessage> getAllProblems() {
         HttpHeaders headers= new HttpHeaders();
         headers.setContentType(new MediaType("application","json", Charset.forName("UTF-8")));
         Map<String,Object> responseMap = new HashMap<>();
 
         responseMap.put("problems",problems);
 
-        ResponseMessage responseMessage = new ResponseMessage(200, "조회 성공", responseMap);
+        ResponseMessage responseMessage = new ResponseMessage(200, "전체 문제 조회 성공", responseMap);
         return new ResponseEntity<>(responseMessage, headers, HttpStatus.OK);
     }
 
+    @GetMapping("schedules/{scheduleId}")
+    public ResponseEntity<ResponseMessage> getProblemsByScheduleId(@PathVariable("scheduleId") long scheduleId) {
+        HttpHeaders headers= new HttpHeaders();
+        headers.setContentType(new MediaType("application","json", Charset.forName("UTF-8")));
+        Map<String,Object> responseMap = new HashMap<>();
+        List<ProblemDTO> problemsByScheduleId = problems.stream().filter(problem->problem.getScheduleId() == scheduleId).collect(Collectors.toList());
+        responseMap.put("problemsByScheduleId",problemsByScheduleId);
+
+        ResponseMessage responseMessage = new ResponseMessage(200,"스케쥴 아이디로 문제 조회 성공", responseMap);
+        return new ResponseEntity<>(responseMessage, headers, HttpStatus.OK);
+    }
+
+    @GetMapping("schedules/{scheduleId}/participants/{participantId}")
+    public ResponseEntity<ResponseMessage> getProblemsByParticipantIdAndScheduleId(@PathVariable("scheduleId") long scheduleId, @PathVariable("participantId") long participantId) {
+        HttpHeaders headers= new HttpHeaders();
+        headers.setContentType(new MediaType("application","json", Charset.forName("UTF-8")));
+        Map<String,Object> responseMap = new HashMap<>();
+        List<ProblemDTO> problemsByParticipantIdAndScheduleId = problems.stream().filter(problem->(problem.getParticipantId() == participantId) && problem.getScheduleId()==scheduleId).collect(Collectors.toList());
+
+        responseMap.put("problemsByParticipantIdAndScheduleId",problemsByParticipantIdAndScheduleId);
+
+        ResponseMessage responseMessage = new ResponseMessage(200,"스케쥴 아이디와 참가자 아이디로 문제 조회 성공", responseMap);
+        return new ResponseEntity<>(responseMessage, headers, HttpStatus.OK);
+    }
 
 }
