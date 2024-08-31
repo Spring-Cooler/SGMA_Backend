d30cd6601489e2fc2b9c2df3aabe14037fbf4485
Seohyun Kim <153896425+1etterh@users.noreply.github.com>
2024-08-31 17:57:41 +0900

    Feat: Composite key by embeddable

--- src/main/java/com/springcooler/sgma/submittedanswer/command/domain/aggregate/SubmittedAnswerPK.java
+++ src/main/java/com/springcooler/sgma/submittedanswer/command/domain/aggregate/SubmittedAnswerPK.java
@@ -1,2 +1,21 @@
-package com.springcooler.sgma.submittedanswer.command.domain.aggregate;public class SubmittedAnswerPK {
+package com.springcooler.sgma.submittedanswer.command.domain.aggregate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Embeddable
+public class SubmittedAnswerPK implements Serializable {
+    @Column(name = "problem_id")
+    private long problemId;
+
+    @Column(name="participant_id")
+    private long participantId;
 }
