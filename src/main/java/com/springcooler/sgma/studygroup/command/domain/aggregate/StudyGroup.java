fe3a53f134314af49b1d9e76d0d962daa468d50d
Kibeom Jeon <kibeom1145@gmail.com>
2024-09-01 22:43:01 +0900

    Refactor: 스터디그룹 코드 리팩토링

--- src/main/java/com/springcooler/sgma/studygroup/command/domain/aggregate/StudyGroup.java
+++ src/main/java/com/springcooler/sgma/studygroup/command/domain/aggregate/StudyGroup.java
@@ -1,5 +1,6 @@
 package com.springcooler.sgma.studygroup.command.domain.aggregate;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -15,21 +16,27 @@ public class StudyGroup {
     @Id
     @Column(name="GROUP_ID")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonProperty("group_id")
     private long groupId;
 
     @Column(name="GROUP_NAME")
+    @JsonProperty("group_name")
     private String groupName;
 
     @Column(name="ACTIVE_STATUS")
+    @JsonProperty("active_status")
     private String activeStatus;
 
     @Column(name="GROUP_MEMBERS")
+    @JsonProperty("group_members")
     private int groupMembers;
 
     @Column(name="USER_ID")
+    @JsonProperty("user_id")
     private long userId;
 
     @Column(name="STUDY_GROUP_CATEGORY_ID")
+    @JsonProperty("category_id")
     private int studyGroupCategoryId;
 
 }
