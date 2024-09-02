8122e2d320bba613fc111dc7560534f52fe2afcb
1etterh <153896425+1etterh@users.noreply.github.com>
2024-09-02 16:43:41 +0900

    chore: Common Error Code 추가 및 시간확인용 인터페이스 함수 생성

--- src/main/java/com/springcooler/sgma/problem/command/infrastructure/service/InfraProblemService.java
+++ src/main/java/com/springcooler/sgma/problem/command/infrastructure/service/InfraProblemService.java
@@ -1,7 +1,9 @@
 package com.springcooler.sgma.problem.command.infrastructure.service;
 
+import java.sql.Timestamp;
 import java.util.Map;
 
 public interface InfraProblemService {
     int requestRegistChoices(long problemId, String[] choices);
+    Timestamp requestScheduleStartTime(long problemId);
 }
--- src/main/java/com/springcooler/sgma/problem/command/infrastructure/service/InfraProblemServiceImpl.java
+++ src/main/java/com/springcooler/sgma/problem/command/infrastructure/service/InfraProblemServiceImpl.java
@@ -2,8 +2,11 @@ package com.springcooler.sgma.problem.command.infrastructure.service;
 
 import com.springcooler.sgma.choice.command.application.service.AppChoiceService;
 import com.springcooler.sgma.choice.command.domain.aggregate.vo.ProblemVO;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.sql.Timestamp;
 
 @Service
 public class InfraProblemServiceImpl implements InfraProblemService {
@@ -15,8 +18,15 @@ public class InfraProblemServiceImpl implements InfraProblemService {
         this.appChoiceService = appChoiceService;
     }
 
+    @Transactional
     @Override
     public int requestRegistChoices(long problemId, String[] choices) {
         return appChoiceService.registChoices(new ProblemVO(problemId, choices));
+    }
+
+
+    @Override
+    public Timestamp requestScheduleStartTime(long problemId) {
+        return null;
     }
 }
