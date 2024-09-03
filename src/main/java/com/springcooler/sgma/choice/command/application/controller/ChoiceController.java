53605234a5e6ce388483de17a13b1868ab8fdd7a
1etterh <153896425+1etterh@users.noreply.github.com>
2024-09-03 12:46:40 +0900

    Refactor: 문제 및 선지 등록 API 등록

--- src/main/java/com/springcooler/sgma/choice/command/application/controller/ChoiceController.java
+++ src/main/java/com/springcooler/sgma/choice/command/application/controller/ChoiceController.java
@@ -1,4 +1,21 @@
 package com.springcooler.sgma.choice.command.application.controller;
 
+import com.springcooler.sgma.choice.command.application.service.AppChoiceService;
+import com.springcooler.sgma.problem.common.ResponseDTO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/problem-choices")
 public class ChoiceController {
+    private final AppChoiceService appChoiceService;
+
+    @Autowired
+    public ChoiceController(AppChoiceService appChoiceService) {
+        this.appChoiceService = appChoiceService;
+    }
+
 }
