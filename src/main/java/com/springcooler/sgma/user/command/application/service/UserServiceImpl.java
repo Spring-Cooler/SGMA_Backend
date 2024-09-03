ce4c0f850722cde6030535b77a766795390c0d2e
minseokKim6823 <alstjr6823@naver.com>
2024-09-03 09:22:21 +0900

    Feat : 30초 간격으로 체크 후 만료시 비활성화 #124

--- src/main/java/com/springcooler/sgma/user/command/application/service/UserServiceImpl.java
+++ src/main/java/com/springcooler/sgma/user/command/application/service/UserServiceImpl.java
@@ -1,79 +1,83 @@
-//package com.springcooler.sgma.user.command.application.service;
-//
-//import com.amazonaws.AmazonClientException;
-//import com.amazonaws.services.s3.AmazonS3Client;
-//import com.amazonaws.services.s3.model.CannedAccessControlList;
-//import com.amazonaws.services.s3.model.DeleteObjectRequest;
-//import com.amazonaws.services.s3.model.PutObjectRequest;
-//import com.springcooler.sgma.user.command.application.dto.RequestUpdateUserDTO;
-//import com.springcooler.sgma.user.command.domain.aggregate.vo.RequestUpdateUserVO;
-//import com.springcooler.sgma.user.command.domain.aggregate.UserEntity;
-//import com.springcooler.sgma.user.command.domain.repository.UserRepository;
-//import com.springcooler.sgma.user.common.exception.CommonException;
-//import com.springcooler.sgma.user.common.exception.ErrorCode;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.stereotype.Service;
-//import org.springframework.web.multipart.MultipartFile;
-//
-//import java.io.File;
-//import java.io.FileOutputStream;
-//import java.io.IOException;
-//import java.util.Optional;
-//
-//@Service("userCommandServiceImpl")
-//@Slf4j
-//public class UserServiceImpl implements UserService {
-//
-//    private final UserRepository userRepository;
+package com.springcooler.sgma.user.command.application.service;
+
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.springcooler.sgma.user.command.application.dto.RequestUpdateUserDTO;
+import com.springcooler.sgma.user.command.domain.aggregate.vo.RequestUpdateUserVO;
+import com.springcooler.sgma.user.command.domain.aggregate.UserEntity;
+import com.springcooler.sgma.user.command.domain.repository.UserRepository;
+import com.springcooler.sgma.user.common.exception.CommonException;
+import com.springcooler.sgma.user.common.exception.ErrorCode;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Optional;
+
+@Service("userCommandServiceImpl")
+@Slf4j
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
 //    private final AmazonS3Client s3Client;
 //
 //    @Value("${cloud.aws.s3.bucket}")
 //    private String bucket;
-//
-//    @Autowired
+
+    //    @Autowired
 //    public UserServiceImpl(UserRepository userRepository, AmazonS3Client s3Client) {
 //        this.userRepository = userRepository;
 //        this.s3Client = s3Client;
 //    }
-//
-//    /**
-//     * 사용자 계정을 비활성화하는 메서드.
-//     */
-//    @Override
-//    public UserEntity deactivateUser(Long userId) {
-//        Optional<UserEntity> user = userRepository.findById(userId);
-//        if (user.isEmpty()) {
-//            throw new CommonException(ErrorCode.NOT_FOUND_USER);
-//        }
-//        UserEntity userEntity = user.get();
-//        userEntity.deactivateUser();  // 사용자 상태를 비활성화(INACTIVE)로 변경
-//        return userRepository.save(userEntity);  // 변경된 상태를 저장
-//    }
-//
-//    /**
-//     * 사용자 계정을 활성화하는 메서드.
-//     */
-//    @Override
-//    public UserEntity activateUser(Long userId) {
-//        Optional<UserEntity> user = userRepository.findById(userId);
-//        if (user.isEmpty()) {
-//            throw new CommonException(ErrorCode.NOT_FOUND_USER);
-//        }
-//        UserEntity userEntity = user.get();
-//        userEntity.activateUser();  // 사용자 상태를 활성화(ACTIVE)로 변경
-//        return userRepository.save(userEntity);  // 변경된 상태를 저장
-//    }
-//
-//    /**설명.
-//     *  사용자의 프로필(닉네임, 이미지)을 업데이트하는 메서드.
-//     *설명.
-//     * @param userId 프로필을 업데이트할 사용자의 ID
-//     * @param userUpdateDTO 업데이트할 정보가 담긴 DTO 객체
-//     * @return 업데이트된 UserEntity 객체
-//     * @throws CommonException 사용자가 존재하지 않을 경우 발생
-//     */
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    /**
+     * 사용자 계정을 비활성화하는 메서드.
+     */
+    @Override
+    public UserEntity deactivateUser(Long userId) {
+        Optional<UserEntity> user = userRepository.findById(userId);
+        if (user.isEmpty()) {
+            throw new CommonException(ErrorCode.NOT_FOUND_USER);
+        }
+        UserEntity userEntity = user.get();
+        userEntity.deactivateUser();  // 사용자 상태를 비활성화(INACTIVE)로 변경
+        return userRepository.save(userEntity);  // 변경된 상태를 저장
+    }
+
+    /**
+     * 사용자 계정을 활성화하는 메서드.
+     */
+    @Override
+    public UserEntity activateUser(Long userId) {
+        Optional<UserEntity> user = userRepository.findById(userId);
+        if (user.isEmpty()) {
+            throw new CommonException(ErrorCode.NOT_FOUND_USER);
+        }
+        UserEntity userEntity = user.get();
+        userEntity.activateUser();  // 사용자 상태를 활성화(ACTIVE)로 변경
+        return userRepository.save(userEntity);  // 변경된 상태를 저장
+    }
+
+    /**설명.
+     *  사용자의 프로필(닉네임, 이미지)을 업데이트하는 메서드.
+     *설명.
+     * @param userId 프로필을 업데이트할 사용자의 ID
+     * @param userUpdateDTO 업데이트할 정보가 담긴 DTO 객체
+     * @return 업데이트된 UserEntity 객체
+     * @throws CommonException 사용자가 존재하지 않을 경우 발생
+     */
 //    @Override
 //    public UserEntity updateProfile(Long userId, RequestUpdateUserDTO userUpdateDTO) {
 //        Optional<UserEntity> user = userRepository.findById(userId);
@@ -172,4 +176,4 @@
 //        }
 //        return convertedFile;
 //    }
-//}
+}
