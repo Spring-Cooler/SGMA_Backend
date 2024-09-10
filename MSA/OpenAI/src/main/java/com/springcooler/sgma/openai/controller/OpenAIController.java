88569b00aadd1225d8fd0f74bf75657a73373724
woodart8 <kibeom1145@gmail.com>
2024-09-10 17:37:36 +0900

    Refactor: 프롬프트 수정, Eureka Client로 변경

--- MSA/OpenAI/src/main/java/com/springcooler/sgma/openai/controller/OpenAIController.java
+++ MSA/OpenAI/src/main/java/com/springcooler/sgma/openai/controller/OpenAIController.java
@@ -20,10 +20,12 @@ public class OpenAIController {
     }
 
     @GetMapping("/grade")
-    public String chat(@RequestParam(name = "answer") String answer,
+    public String chat(@RequestParam(name = "problem") String problem,
+                       @RequestParam(name = "answer") String answer,
                        @RequestParam(name = "submitted-answer") String submittedAnswer) {
-        String prompt = answer + "가 정답이고" + submittedAnswer +
-                "가 제출답안인데 정답여부에 대해 O또는 X로만 대답해줘";
+        String prompt = "\"" + problem + "\"라는 문제와 \"" + answer + "\"라는 정답이 있을 때" +
+                "\"" + submittedAnswer + "\"가 의미상 또는 수치상 정답이면 'O', 아니면 'X'로 간단한 설명과 함께 판정해줘"
+                + ",단, 대답은 판정 결과 1글자와 설명 1줄을 ','로 구별해서 줄바꿈 없이 대답해줘";
         ResponseChatDTO res = openAIService.getGrade(prompt);
         return res.getChoices().get(0).getMessage().getContent();
     }
