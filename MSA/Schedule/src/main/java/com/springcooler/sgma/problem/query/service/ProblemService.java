efbbf80865149a45d93beb0978ff91e6d5578bf9
1etterh <153896425+1etterh@users.noreply.github.com>
2024-09-11 13:50:28 +0900

    Refactor: 주관식 반영하기 위해 getAnswerByProblemID 삭제

--- MSA/Schedule/src/main/java/com/springcooler/sgma/problem/query/service/ProblemService.java
+++ MSA/Schedule/src/main/java/com/springcooler/sgma/problem/query/service/ProblemService.java
@@ -7,14 +7,12 @@ import java.util.List;
 public interface ProblemService {
 
     // 문제 전체 조회
-    public List<ProblemDTO> findAllProblems();
+    List<ProblemDTO> findAllProblems();
 
     // ID로 문제 조회
-    public ProblemDTO findProblemById(Long id);
+    ProblemDTO findProblemById(Long id);
 
     // 스케쥴 ID로 문제 조회
-    public List<ProblemDTO> findProblemsByScheduleId(Long scheduleId);
+    List<ProblemDTO> findProblemsByScheduleId(Long scheduleId);
 
-
-    String getAnswerByProblemId(Long problemId);
 }
