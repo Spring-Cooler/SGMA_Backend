59511c23c91c48e659dfb41afac2e66a7a06d219
1etterh <153896425+1etterh@users.noreply.github.com>
2024-09-12 16:13:31 +0900

    Chore: test code에 @Transactional 추가,  problemInfo에 choice 정보 추가

--- MSA/Schedule/src/test/java/com/springcooler/sgma/problem/command/application/service/AppProblemServiceTests.java
+++ MSA/Schedule/src/test/java/com/springcooler/sgma/problem/command/application/service/AppProblemServiceTests.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,6 +15,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Slf4j
+@Transactional
 class AppProblemServiceTests {
 
     @Autowired
