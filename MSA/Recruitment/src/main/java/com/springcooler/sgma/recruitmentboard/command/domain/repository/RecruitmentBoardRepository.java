861d7436cfcb83eb120c4cc955e602008872bca6
minseokKim6823 <alstjr6823@naver.com>
2024-09-30 12:41:07 +0900

    좋아요 테스트 중

--- MSA/Recruitment/src/main/java/com/springcooler/sgma/recruitmentboard/command/domain/repository/RecruitmentBoardRepository.java
+++ MSA/Recruitment/src/main/java/com/springcooler/sgma/recruitmentboard/command/domain/repository/RecruitmentBoardRepository.java
@@ -7,9 +7,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface RecruitmentBoardRepository extends JpaRepository<RecruitmentBoard,Long> {
 
     List<RecruitmentBoard> findByActiveStatus(BoardActiveStatus status);
+
+    Optional<RecruitmentBoard> findByBoardIdAndUserId(Long recruitmentBoardId,Long UserId);
 }
